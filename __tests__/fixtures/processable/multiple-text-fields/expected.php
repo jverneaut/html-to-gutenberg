a258c610ed6812771513b9f63a681cc54257f90d
Julien Verneaut <jverneaut@gmail.com>
2025-04-05 00:46:48 +0200

    Use less restrictive wp_kses_post filter

--- __tests__/fixtures/processable/multiple-text-fields/expected.php
+++ __tests__/fixtures/processable/multiple-text-fields/expected.php
@@ -1,4 +1,4 @@
 <section <?php echo get_block_wrapper_attributes(); ?>>
-  <h1><?php echo esc_html($attributes['title'] ?? ''); ?></h1>
-  <p><?php echo esc_html($attributes['content'] ?? ''); ?></p>
+  <h1><?php echo wp_kses_post($attributes['title'] ?? ''); ?></h1>
+  <p><?php echo wp_kses_post($attributes['content'] ?? ''); ?></p>
 </section>
--- example/generated/demo-block/render.php
+++ example/generated/demo-block/render.php
@@ -7,9 +7,9 @@ $image_alt = get_post_meta($attributes['image'], '_wp_attachment_image_alt', tru
 <section <?php echo get_block_wrapper_attributes(['class' => 'container']); ?>>
   <div class="grid grid-cols-12 px-8 gap-x-6">
     <div class="col-span-6 flex flex-col justify-center">
-      <h1><?php echo esc_html($attributes['title'] ?? ''); ?></h1>
+      <h1><?php echo wp_kses_post($attributes['title'] ?? ''); ?></h1>
 
-      <p><?php echo esc_html($attributes['content'] ?? ''); ?></p>
+      <p><?php echo wp_kses_post($attributes['content'] ?? ''); ?></p>
     </div>
 
     <div class="col-span-6">
