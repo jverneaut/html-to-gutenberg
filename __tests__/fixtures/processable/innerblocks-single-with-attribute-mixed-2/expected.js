bf251a417eb108925cabded1c16ed673be60a49f
Julien Verneaut <jverneaut@gmail.com>
2025-04-04 18:27:28 +0200

    Add additionnal test cases for attirbutes priorities

---
+++ __tests__/fixtures/processable/innerblocks-single-with-attribute-mixed-2/expected.js
@@ -0,0 +1,14 @@
+import { useBlockProps, InnerBlocks } from "@wordpress/block-editor";
+
+export default () => {
+  return (
+    <section {...useBlockProps()}>
+      <InnerBlocks
+        allowedBlocks={["custom/child-block"]}
+        template={[
+          ["custom/child-block", { title: "Has priority over props" }],
+        ]}
+      ></InnerBlocks>
+    </section>
+  );
+};
