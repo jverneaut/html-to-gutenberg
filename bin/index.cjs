4ab764141cf49c4224e5cd1d381714a1cc93eb75
Julien Verneaut <jverneaut@gmail.com>
2025-04-05 18:04:16 +0200

    Fix package name

--- bin/index.cjs
+++ bin/index.cjs
@@ -9,7 +9,7 @@ const packageJSON = require("../package.json");
 const HTMLToGutenberg = require("../src/HTMLToGutenberg.js").default;
 
 program
-  .name("npx " + packageJSON.name)
+  .name("npx " + packageJSON.name.split("/")[1])
   .description(packageJSON.description)
   .version(packageJSON.version)
 
