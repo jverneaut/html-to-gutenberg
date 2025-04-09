1a028a722005cb4e275e79c32dcbc00bb67b40eb
Julien Verneaut <jverneaut@gmail.com>
2025-04-09 11:48:50 +0200

    Update example to handle not set images

--- example/generated/demo-block/render.php
+++ example/generated/demo-block/render.php
@@ -1,7 +1,10 @@
 <?php
 
-$image = wp_get_attachment_image_src($attributes['image'], 'full');
-$image_alt = get_post_meta($attributes['image'], '_wp_attachment_image_alt', true);
+$image_id = $attributes['image'] ?? '';
+$image = $image_id ? wp_get_attachment_image_src($image_id, 'full') : [''];
+$image_url = $image[0] ?? '';
+$image_alt = $image_id ? get_post_meta($image_id, '_wp_attachment_image_alt', true) : '';
+
 ?>
 
 <section <?php echo get_block_wrapper_attributes(['class' => 'container']); ?>>
