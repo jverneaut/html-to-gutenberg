ce6ab8845db1dee7560f3db92c5897adf3334598
Julien Verneaut <jverneaut@gmail.com>
2025-04-09 11:43:29 +0200

    Update tests to handle not set images

--- __tests__/fixtures/processable/multiple-image-fields/expected.php
+++ __tests__/fixtures/processable/multiple-image-fields/expected.php
@@ -1,10 +1,15 @@
 <?php
 
-$image_1 = wp_get_attachment_image_src($attributes['image_1'], 'full');
-$image_1_alt = get_post_meta($attributes['image_1'], '_wp_attachment_image_alt', true);
+$image_1_id = $attributes['image_1'];
+$image_1 = $image_1_id ? wp_get_attachment_image_src($image_1_id, 'full') : [''];
+$image_1_url = $image_1[0] ?? '';
+$image_1_alt = $image_1_id ? get_post_meta($image_1_id, '_wp_attachment_image_alt', true) : '';
 
-$image_2 = wp_get_attachment_image_src($attributes['image_2'], 'full');
-$image_2_alt = get_post_meta($attributes['image_2'], '_wp_attachment_image_alt', true);
+$image_2_id = $attributes['image_2'];
+$image_2 = $image_2_id ? wp_get_attachment_image_src($image_2_id, 'full') : [''];
+$image_2_url = $image_2[0] ?? '';
+$image_2_alt = $image_2_id ? get_post_meta($image_2_id, '_wp_attachment_image_alt', true) : '';
+
 ?>
 
 <section <?php echo get_block_wrapper_attributes(); ?>>
