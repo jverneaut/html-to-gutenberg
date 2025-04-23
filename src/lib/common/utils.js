182d12cc5152f0030b8434e784c6c1cf8676c740
Julien Verneaut <jverneaut@gmail.com>
2025-04-23 18:00:25 +0200

    Parse array of strings as space separated string to handle classNames

--- src/lib/common/utils.js
+++ src/lib/common/utils.js
@@ -5,12 +5,25 @@ export const spaceSeparatedStringToArray = (str) => str.split(/\s+/);
 // Parses a raw string value into its correct type (e.g., string, number, boolean)
 // If parsing fails, returns the original string
 export const parseRawValue = (rawValue) => {
+  let parsedValue = null;
   let value = null;
 
+  // First, try to convert array to string if possible
   try {
-    value = JSON.parse(rawValue);
+    if (Array.isArray(rawValue)) {
+      parsedValue = rawValue.join(" ");
+    } else {
+      parsedValue = rawValue;
+    }
   } catch {
-    value = rawValue;
+    parsedValue = rawValue;
+  }
+
+  // Then, try to parse the value
+  try {
+    value = JSON.parse(parsedValue);
+  } catch {
+    value = parsedValue;
   }
 
   return value;
