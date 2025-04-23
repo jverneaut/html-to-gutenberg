7ae6bc533b8159596a46534e8529302e39bfc4fa
Julien Verneaut <jverneaut@gmail.com>
2025-04-23 20:57:42 +0200

    Add test for image with data-image-size attribute

---
+++ __tests__/fixtures/processable/single-image-field-with-size/expected.php
@@ -0,0 +1,14 @@
+<?php
+
+$image_id = $attributes['image'] ?? '';
+$image = $image_id ? wp_get_attachment_image_src($image_id, 'medium') : [''];
+$image_src = $image[0] ?? '';
+$image_srcset = $image_id ? wp_get_attachment_image_srcset($image_id, 'medium') : '';
+$image_sizes = $image_id ? wp_get_attachment_image_sizes($image_id, 'medium') : '';
+$image_alt = $image_id ? get_post_meta($image_id, '_wp_attachment_image_alt', true) : '';
+
+?>
+
+<section <?php echo get_block_wrapper_attributes(); ?>>
+  <img src="<?php echo esc_url($image_src); ?>" srcset="<?php echo esc_attr($image_srcset); ?>" sizes="<?php echo esc_attr($image_sizes); ?>" alt="<?php echo esc_attr($image_alt); ?>" />
+</section>
