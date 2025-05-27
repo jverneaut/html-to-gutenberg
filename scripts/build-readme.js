ae0a1b01cf9293240b267d964780e306c339821b
Julien Verneaut <jverneaut@gmail.com>
2025-05-27 16:10:31 +0200

    Add introduction to README

--- scripts/build-readme.js
+++ scripts/build-readme.js
@@ -28,6 +28,12 @@ const convertTemplate = (template) => {
   return convertedTemplate;
 };
 
+const introductionTemplate = convertTemplate(
+  fs.readFileSync("docs/docs/common/_introduction.md", {
+    encoding: "utf-8",
+  }),
+);
+
 const quickStartTemplate = convertTemplate(
   fs.readFileSync("docs/docs/common/_quick-start.md", {
     encoding: "utf-8",
@@ -36,6 +42,10 @@ const quickStartTemplate = convertTemplate(
 
 const template = `![Tests Status](https://github.com/jverneaut/html-to-gutenberg/actions/workflows/test.yml/badge.svg)
 ![GitHub Release](https://img.shields.io/github/v/release/jverneaut/html-to-gutenberg)
+
+<img src="./docs/static/img/logo.svg" alt="HTML To Gutenberg" width="100">
+
+# HTML To Gutenberg
 
 ## Quick start
 
