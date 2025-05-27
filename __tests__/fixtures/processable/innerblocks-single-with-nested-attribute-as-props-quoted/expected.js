be680825c301bdb65e6190136ce85bf740a23028
Julien Verneaut <jverneaut@gmail.com>
2025-05-27 04:26:25 +0200

    Add test for quoted values

---
+++ __tests__/fixtures/processable/innerblocks-single-with-nested-attribute-as-props-quoted/expected.js
@@ -0,0 +1,24 @@
+import { useBlockProps, InnerBlocks } from "@wordpress/block-editor";
+
+export default () => {
+  return (
+    <section {...useBlockProps()}>
+      <InnerBlocks
+        allowedBlocks={["custom/child-block"]}
+        template={[
+          [
+            "custom/child-block",
+            { content: "'test ' '''\\'''" },
+            [
+              [
+                "custom/child-block",
+                { content: "'test ' '''\\'''" },
+                [["custom/child-block", { content: '"test " """\\""""' }]],
+              ],
+            ],
+          ],
+        ]}
+      ></InnerBlocks>
+    </section>
+  );
+};
