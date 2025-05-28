76e39e717d77ac483d8f5fc5ffc3ab58591a2f74
Julien Verneaut <jverneaut@gmail.com>
2025-05-28 23:22:56 +0200

    Update @jverneaut/gutenberg-webpack-plugin

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -16,20 +16,4 @@ export default {
     // Optionally use this webpack plugin to build the blocks instead of @wordpress/scripts
     new GutenbergWebpackPlugin("./generated"),
   ],
-
-  module: {
-    rules: [
-      {
-        // Handle React JSX with babel-loader
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-    ],
-  },
 };
