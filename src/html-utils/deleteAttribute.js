de6c099d968af69a6e5a64e0a80865ea4145d9bb
Julien Verneaut <jverneaut@gmail.com>
2025-06-02 19:01:10 +0200

    Remove old code

--- src/html-utils/deleteAttribute.js
+++
@@ -1,9 +0,0 @@
-import { visit } from "unist-util-visit";
-
-export default (ast, attributeName) => {
-  visit(ast, "element", (node) => {
-    if (node.properties[attributeName]) {
-      delete node.properties[attributeName];
-    }
-  });
-};
--- src/html-utils/deleteTagName.js
+++
@@ -1,5 +0,0 @@
-import { remove } from "unist-util-remove";
-
-export default (ast, tagName) => {
-  remove(ast, (node) => node.tagName === tagName);
-};
