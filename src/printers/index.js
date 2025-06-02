2f3c13c15713b0a3c93c8da34efb96570244d830
Julien Verneaut <jverneaut@gmail.com>
2025-06-02 15:06:14 +0200

    Export all printers

--- src/printers/index.js
+++ src/printers/index.js
@@ -1,1 +1,11 @@
-export default [];
+import PrinterBlockJSON from "./PrinterBlockJSON.js";
+import PrinterEditJS from "./PrinterEditJS.js";
+import PrinterIndexJS from "./PrinterIndexJS.js";
+import PrinterRenderPHP from "./PrinterRenderPHP.js";
+
+export default [
+  PrinterBlockJSON,
+  PrinterEditJS,
+  PrinterIndexJS,
+  PrinterRenderPHP,
+];
