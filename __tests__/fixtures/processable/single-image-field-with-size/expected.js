71dfb9c25d3fdc27b42ecc393f44f6f91d8e6ae7
Julien Verneaut <jverneaut@gmail.com>
2025-06-04 01:07:09 +0200

    Rename Image component import

--- __tests__/fixtures/processable/single-image-field-with-size/expected.js
+++ __tests__/fixtures/processable/single-image-field-with-size/expected.js
@@ -1,5 +1,5 @@
 import { useBlockProps, MediaUpload } from "@wordpress/block-editor";
-import { Image } from "@10up/block-components";
+import { Image } from "@10up/block-components/components/image";
 
 export default ({ attributes, setAttributes }) => {
   return (
