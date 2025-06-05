8cbcda66d38e1709cb92b65fb4d76af8b6c34d3d
Julien Verneaut <jverneaut@gmail.com>
2025-06-05 21:17:36 +0200

    Remove : in JSX attributes

--- src/processors/global/GlobalJSXAttributes.js
+++ src/processors/global/GlobalJSXAttributes.js
@@ -5,13 +5,19 @@ export default class GlobalJSXAttributes extends ProcessorBase {
   processHTMLStringByFilename(filename, htmlString) {
     if (filename === PrinterEditJS.filename) {
       // FIXME: This is very brittle in my opinion, maybe we should look into proper estree manipulation
-      return htmlString
-        .replaceAll("class=", "className=")
-        .replace(/\s(on[a-z]+)=/g, (_, event) => {
-          const reactEvent =
-            "on" + event.charAt(2).toUpperCase() + event.slice(3);
-          return ` ${reactEvent}=`;
-        });
+      return (
+        htmlString
+          // React uses class instead of className
+          .replaceAll("class=", "className=")
+          // Remove attributes that contains a ":", like xml:space, this is not valid JSX
+          .replace(/(\s)([^=\s"]*):([^=\s"]*)=/g, "$1$2$3=")
+          // Capitalize the first letter of event handlers, like onChange, onClick, etc.
+          .replace(/\s(on[a-z]+)=/g, (_, event) => {
+            const reactEvent =
+              "on" + event.charAt(2).toUpperCase() + event.slice(3);
+            return ` ${reactEvent}=`;
+          })
+      );
     }
 
     return htmlString;
