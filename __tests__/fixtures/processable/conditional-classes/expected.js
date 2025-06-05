7d027a13b326f7bf0aec323cd982e8776d896861
Julien Verneaut <jverneaut@gmail.com>
2025-06-05 11:55:29 +0200

    Fix test output

--- __tests__/fixtures/processable/conditional-classes/expected.js
+++ __tests__/fixtures/processable/conditional-classes/expected.js
@@ -1,6 +1,13 @@
 import { useBlockProps } from "@wordpress/block-editor";
+import { useSelect } from "@wordpress/data";
+import { useEntityProp } from "@wordpress/core-data";
 
 export default ({ attributes, setAttributes }) => {
+  const postType = useSelect(
+    (select) => select("core/editor").getCurrentPostType(),
+    [],
+  );
+
   const [meta, setMeta] = useEntityProp("postType", postType, "meta");
 
   return (
